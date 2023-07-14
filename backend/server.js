a2c79cf80d2a1f86b0b98a2270ef0a041b9e35cb
Lirae Que Data <liraedata59@gmail.com>
2023-07-14 09:50:41 +0800

    Added backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+
+dotenv.config();
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended : true
+}));
+app.use(cookieParser());
+
+
+app.listen(port, () => {
+    console.log(`Server running on http://127.0.0.1:${port}/`);
+});
